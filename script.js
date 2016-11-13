716586311d5fb85b6336136679db42805038b890
erikslewis <adelina198527@gmail.com>
2016-11-13 15:31:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,34 @@ var cardtwo = "queen";
 var cardthree = "king";
 var cardfour = "king";
 
-if (cardOne === cardtwo) {
+var gameBoard = document.getElementById("game-board");
+
+
+
+cards = ['cardOne','cardtwo','cardthree','cardfour'];
+
+
+for (i = 0; i < cards.length; i++){
+	var x = document.createElement("div");
+	x.className="card";
+	gameBoard.appendChild(x);
+
+}
+
+var createCards = cards.filter(function(x) {
+  for (i = 0; i < cards.length; i++)
+  if (x[i]['cardOne'] === cardtwo || x[i]['cardthree'] === cardfour) {
+      return cards[i];
+  }
+})
+
+
+
+	 
+  
+// 
+
+/*if (cardOne === cardtwo) {
 alert ("You found a match!");
 } 
 else { 
@@ -18,6 +45,6 @@ alert("You found a match!");
 }
 else {
 alert("sorry try again!");
-}	
+}	*/
 
 
