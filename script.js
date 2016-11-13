2ae1e5eac97c7154d1314efcf78189e6b45209b7
erikslewis <adelina198527@gmail.com>
2016-11-12 19:43:35 -0500

    Add files via upload
    2nd update

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+
+var cardOne = "queen";
+var cardtwo = "queen";
+var cardthree = "king";
+var cardfour = "king";
+
+if (cardOne === cardtwo) {
+alert ("You found a match!");
+} 
+else { 
+alert("sorry try again!");
+}
+if (cardthree === cardfour) {
+alert("You found a match!");
+}
+else {
+alert("sorry try again!");
+}	
+
+
